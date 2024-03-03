9f34d17120dcdbc622666ab3fc0465d34d60dcd2
mnrval <rasyidajha1140@gmail.com>
2024-03-03 23:22:01 +0700

    jualbakso

---
+++ okaw.js
@@ -0,0 +1,90 @@
+// Buat Lu Yang Jual Sc Ini Gw Tonjok 
+
+
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+global.gr = 'https://wa.me/6288210180505?text=bang+aku+mau+donasi+i+love+yuuu'
+global.ig = 'https://instagram/@zal_x_u' // ubah aja
+global.ofc = 'https://chat.whatsapp.com/CAaHTC4unnCCcZN70qRRgZ'
+global.okta = 'https://taaofc-panel.my.id'
+global.gh = 'https://github.com/chataofc'
+global.saluran = 'https://whatsapp.com/channel/0029VaMXy2I0wak0RDGOzH3z'
+global.email = 'rizaldev@gmail.com' //serah
+global.region = 'indonesia' // serah
+global.dana = '088210180505'
+global.gopay = '088210180505'
+global.pulsa = '088210180505'
+//—————「 Set Nama Own & Bot 」—————//
+global.ownername = 'nyobain' //ubah jadi nama mu, note tanda ' gausah di hapus!
+//=================================================//
+global.owner = ['6281387719373'] // ubah aja pake nomor lu
+global.premium = ['6281387719373']
+//==========================BY Hw Mods=======================//
+global.lolkey = 'a8e86232771f9bc1826742c1'
+global.zenz = 'zenzkey_41b4fe7a5d' // https://api.zahwazein.xyz
+global.keyopenai = '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'
+//====================BY Hw Mods=============================//
+global.botname = 'ᴘʀᴏᴊᴇᴄᴛ ʀɪᴢᴀʟ-ᴅᴇᴠ ❄️' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.packname = 'zalldev' // ubah aja ini nama sticker
+global.ta = '•' //cuma simbol
+global.author = 'ᴘʀᴏᴊᴇᴄᴛ ʀɪᴢᴀʟ-ᴅᴇᴠ ❄️' // ubah aja ini nama sticker
+global.prefa = ['','!','.',',','🐤','🗿']
+global.sessionName = 'oka' //Gausah Juga
+global.sp = '⭔' // Gausah Juga
+global.wlcm = []
+global.wlcmm = []
+global.anticall = true
+//=================================================//
+
+
+//imgnya disini
+global.thum = fs.readFileSync("./baseoka/image/thum.jpeg") 
+global.good = fs.readFileSync("./baseoka/image/good.jpeg") 
+global.vidmenu = fs.readFileSync("./baseoka/video/mainmenu.mp4")
+
+global.mess = {
+    done: '𝐃𝐨𝐧𝐞 𝐊𝐚𝐤 ❄️',
+    admin: '𝐅𝐢𝐭𝐮𝐫 𝐢𝐧𝐢 𝐛𝐮𝐚𝐭 *𝐀𝐝𝐦𝐢𝐧 𝐆𝐫𝐨𝐮𝐩* 𝐤𝐚𝐤❄️',
+    botAdmin: '𝐏𝐞𝐫𝐢𝐧𝐭𝐚𝐡 𝐈𝐧𝐢 𝐇𝐚𝐧𝐲𝐚 𝐁𝐢𝐬𝐚 𝐃𝐢𝐠𝐮𝐧𝐚𝐤𝐚𝐧 𝐊𝐞𝐭𝐢𝐤𝐚 𝐁𝐨𝐭 𝐌𝐞𝐧𝐣𝐚𝐝𝐢 𝐀𝐝𝐦𝐢𝐧 𝐆𝐫𝐨𝐮𝐩 ❄️',
+    owner: '𝐅𝐢𝐭𝐮𝐫 𝐢𝐧𝐢 𝐛𝐮𝐚𝐭 *𝐨𝐰𝐧𝐞𝐫* 𝐤𝐚𝐤❄️',
+    group: '𝐅𝐢𝐭𝐮𝐫 𝐢𝐧𝐢 𝐛𝐮𝐚𝐭 *𝐆𝐫𝐨𝐮𝐩 𝐂𝐡𝐚𝐭* 𝐤𝐚𝐤❄️',
+    private: '𝐅𝐢𝐭𝐮𝐫 𝐢𝐧𝐢 𝐛𝐮𝐚𝐭 *𝐀𝐝𝐦𝐢𝐧 𝐆𝐫𝐨𝐮𝐩* 𝐤𝐚𝐤❄️',
+    wait: 'ᴘʀᴏᴊᴇᴄᴛ ʀɪᴢᴀʟ-ᴅᴇᴠ ❄️ 𝐭𝐮𝐧𝐠𝐠𝐮 𝐬𝐞𝐛𝐞𝐧𝐭𝐚𝐫 𝐲𝐚 𝐤𝐚𝐤',
+    endLimit: '𝐥𝐢𝐦𝐢𝐭 𝐤𝐚𝐦𝐮 𝐬𝐮𝐝𝐚𝐡 𝐡𝐚𝐛𝐢𝐬 𝐤𝐚𝐤, 𝐭𝐮𝐧𝐠𝐠𝐮 𝐣𝐚𝐦 𝟏𝟐 𝐦𝐚𝐥𝐚𝐦 𝐮𝐧𝐭𝐮𝐤 𝐫𝐞𝐬𝐞𝐭❄️',
+    error: '*𝐅𝐢𝐭𝐮𝐫 𝐬𝐞𝐝𝐚𝐧𝐠 𝐞𝐫𝐨𝐫 𝐤𝐚𝐤❄️!!!*',
+    prem : '𝐅𝐢𝐭𝐮𝐫 𝐢𝐧𝐢 𝐛𝐮𝐚𝐭 𝐦𝐞𝐦𝐛𝐞𝐫 𝐩𝐫𝐞𝐦𝐢𝐮𝐦 ᴘʀᴏᴊᴇᴄᴛ ʀɪᴢᴀʟ-ᴅᴇᴠ ❄️'
+}
+//=================================================//
+//Gausah Juga
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+
+//rpg
+global.buruan = {
+   ikan: 5,
+   ayam: 5,
+   kelinci: 5,
+   domba: 5,
+   sapi: 5,
+   gajah: 5
+}
+global.rpg = {
+   darahawal: 100,
+   besiawal: 5,
+   goldawal: 1,
+   emeraldawal: 1,
+   umpanawal: 1,
+   potionawal: 1
+}
+//=================================================//
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
